c6262cd941b59c7ad7333347a7160638820f86cc
Alon Emanuel <alon@alonemanuel.com>
2020-02-24 00:19:59 +0200

    Done

--- cubic_disarray.js
+++ cubic_disarray.js
@@ -11,6 +11,9 @@ ctx.scale(dpr, dpr);
 ctx.lineWidth = 2;
 var SQUARE_SIZE = 30;
 
+var randomDisplacement = 15
+var rotateMultiplier = 20;
+var offset = 10;
 
 function draw(width, height) {
     ctx.beginPath();
@@ -20,16 +23,32 @@ function draw(width, height) {
 
 
 function drawSquares() {
-    numOfSquares = widthAndHeight / SQUARE_SIZE;
     for (let i = SQUARE_SIZE; i <= size - SQUARE_SIZE; i += SQUARE_SIZE) {
         for (let j = SQUARE_SIZE; j <= size - SQUARE_SIZE; j += SQUARE_SIZE) {
             ctx.save();
-            ctx.translate(i + 10, j + 10);
+            var plusOrMinus = Math.random() > 0.5 ? -1 : 1;
+            var rotateAmt = j / size * Math.PI / 180 * plusOrMinus * Math.random() * rotateMultiplier;
+
+            plusOrMinus = Math.random() > 0.5 ? -1 : 1;
+            var translateAmt = j / size * plusOrMinus * Math.random() * randomDisplacement;
+
+            ctx.translate(i + translateAmt + offset, j + offset);
+            ctx.rotate(rotateAmt);
             draw(SQUARE_SIZE, SQUARE_SIZE);
-            ctx.restore()
+            ctx.restore();
         }
     }
 }
 
+function fillBackgroundWhite(context) {
+    // Fill background with white.
+    context.save();
+    context.globalCompositeOperation = 'destination-over';
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.restore();
+}
+
 drawSquares();
+fillBackgroundWhite(ctx);
 
